08baac1ae9648c7c551e56538fad229ff4ee0302
kolvin2016 <kolvin@qq.com>
2016-08-13 13:39:02 +0800

    Add files via upload

--- messages/en-US/widget.php
+++ messages/en-US/widget.php
@@ -1,1 +1,15 @@
-
+<?php
+return [
+    'dual list non selected'=>'Non-Selected',
+    'dual list info text'=>'Showing all',
+    'dual list filter text clear'=>'show all',
+    'dual list filtered'=>'Filtered',
+    'dual list from'=>'from',
+    'dual list filter placeholder'=>'Filter',
+    'dual list move selected label'=>'Move selected',
+    'dual list move all label'=>'Move all',
+    'dual list remove selected label'=>'Remove selected',
+    'dual list remove all label'=>'Remove all',
+    'dual list info text empty'=>'Empty list',
+    'dual list selected list label'=>'Selected',
+];
