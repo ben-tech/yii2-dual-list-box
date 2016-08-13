ae9516b067e942ca4b0e418354d50e84af877d92
kolvin2016 <kolvin@qq.com>
2016-08-13 13:39:23 +0800

    Add files via upload

--- messages/zh-CN/widget.php
+++ messages/zh-CN/widget.php
@@ -1,1 +1,17 @@
-
+<?php
+return [
+    'test'=>'测试',
+    'dual list non selected'=>'未选',
+    'dual list info text'=>'总共',
+    'dual list filter text clear'=>'显示所有',
+    'dual list filtered'=>'过滤',
+    'dual list from'=>'共',
+    'dual list filter placeholder'=>'这里输入过滤',
+    'dual list move selected label'=>'移动选中项',
+    'dual list move all label'=>'移动全部',
+    'dual list remove selected label'=>'移除选中项',
+    'dual list remove all label'=>'移除所有项',
+    'dual list info text empty'=>'无选项',
+    'dual list selected list label'=>'已选',
+];
+?>
